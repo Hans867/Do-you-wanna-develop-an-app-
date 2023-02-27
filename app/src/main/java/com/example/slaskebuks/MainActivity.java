b98b6311b8f283658a51d613f5fc454ad16b3716
Hans867 <hans867m@stud.kea.dk>
2023-02-27 10:58:23 +0100

    Hi! Do you wanna develop an app? 🐸

---
+++ app/src/main/java/com/example/slaskebuks/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.slaskebuks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: App up and running");
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        Log.d(TAG, "onPause: Pausing the app");
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        Log.d(TAG, "onResume: Now resuming the application");
+    }
+}
